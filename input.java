52f831d06df4b3b3a8f82f95f3140d2624a3a0c8
Bhuvaneshwaran Saravanan <bhuvaneshsaravanan100@gmail.com>
2024-09-23 10:00:07 +0000

    Add files via upload

---
+++ input.java
@@ -0,0 +1,20 @@
+package Basics;
+import java.util.Scanner;
+
+public class input {
+	public static void main(String[] args) {
+		Scanner a= new Scanner(System.in);
+
+		
+		System.out.println("Enter the Name:");
+		String b=a.nextLine();
+		System.out.println(b);
+
+		
+	
+		
+
+		
+	}
+
+}
