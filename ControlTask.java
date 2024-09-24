81df61016b830c8db79a6c503db0e60c549de3bc
Bhuvaneshwaran Saravanan <bhuvaneshsaravanan100@gmail.com>
2024-09-24 06:11:15 +0000

    Add files via upload

---
+++ ControlTask.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class ControlTask {
+	private void even(int num) {
+		if(num%2==0) {
+			System.out.println("Even number");
+		}
+		else {
+			System.out.println("Odd number");
+		}
+	}
+	public static void main(String[] args) {
+		ControlTask a = new ControlTask();
+		a.even(7);
+	}
+
+}
---
+++ ifelsejava.java
@@ -0,0 +1,27 @@
+package Basics;
+
+public class ifelsejava {
+
+	private void voter(int age) {
+		if(age>=18){
+			System.out.println("ok");
+		}
+			else{
+				System.out.println("not");
+			}
+		}
+	
+	public static void main(String[] args) {
+		ifelsejava a =new ifelsejava();
+		a.voter(10);
+	}
+
+}
+
+
+
+
+
+
+
+
